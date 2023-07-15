2a0f9bb338178c15f0cc91f614a8f88c6bcc926b
Pankorn Chantarod <pankorn.ch@gmail.com>
2023-07-15 20:21:08 +0700

    add: create_hotel method

--- main.dart
+++ main.dart
@@ -1,7 +1,9 @@
+import 'services/hotel_management_service.dart';
 import 'utils/command.dart';
 
 void main() async {
   String filename = "input.txt";
+  HotelManagementService service = new HotelManagementService();
 
   List<Command> commands = await Command.getCommandsFormFileName(filename);
 
@@ -10,6 +12,8 @@ void main() async {
       case "create_hotel":
         int floor = int.parse(command.params[0]);
         int roomPerFloor = int.parse(command.params[1]);
+        var rooms = service.createHotel(floor, roomPerFloor);
+
         print(
             "Hotel created with $floor floor(s), $roomPerFloor room(s) per floor.");
         return;
--- services/hotel_management_service.dart
+++ services/hotel_management_service.dart
@@ -1,9 +1,30 @@
+import '../models/keycard.dart';
 import '../models/room.dart';
 
 class HotelManagementService {
-  List<Room> rooms = new List.empty();
+  List<Room> rooms = [];
+  List<Keycard> keycards = [];
 
-  createHotel(int floors, int roomsPerFloor) {}
+  List<Room> createHotel(int floors, int roomsPerFloor) {
+    List<Room> _rooms = [];
+    List<Keycard> _keycards = [];
+
+    for (int f = 0; f < floors; f++) {
+      for (int r = 0; r < roomsPerFloor; r++) {
+        Room _room = Room(roomId: createRoomId(f + 1, r + 1), floor: f + 1);
+        _rooms.add(_room);
+
+        Keycard _keycard = new Keycard(keycardId: "${keycards.length + 1}");
+        _keycards.add(_keycard);
+      }
+    }
+
+    this.rooms = _rooms;
+    this.keycards = _keycards;
+
+    return this.rooms;
+  }
+
   bookByUser() {}
   listAvailableRooms() {}
   checkoutByUser() {}
@@ -13,4 +34,8 @@ class HotelManagementService {
   listGuestsByFloor() {}
   bookByFloor() {}
   checkoutGuestByFloor() {}
+
+  static String createRoomId(int floor, int room) {
+    return "$floor${room.toString().padLeft(2, "0")}";
+  }
 }
